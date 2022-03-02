f493938dcf6e003a9b410349efcf28db53d6d06c
GitHub Action <action@github.com>
2022-03-02 16:15:51 +0000

    Updates plugin including built assets

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-9ddde91ca04e06ec69359193b7b6221b8309a32e',
+        'version' => 'dev-9ddde91ca04e06ec69359193b7b6221b8309a32e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9ddde91ca04e06ec69359193b7b6221b8309a32e',
+        'name' => 'mitlibraries/custom-child-theme-post-types',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mitlibraries/custom-child-theme-post-types' => array(
+            'pretty_version' => 'dev-9ddde91ca04e06ec69359193b7b6221b8309a32e',
+            'version' => 'dev-9ddde91ca04e06ec69359193b7b6221b8309a32e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9ddde91ca04e06ec69359193b7b6221b8309a32e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
